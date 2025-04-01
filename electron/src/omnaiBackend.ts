a08cf1955a9f37b2004d927e997dc52526f8e6c7
AKMaily <anna.theimann@outlook.de>
2025-04-01 16:26:55 +0200

    Feature: Starting Backend on a free port
    Problem: For v0.5.0 to start the backend with a websocket connection
    a port has to be given in the CLI.
    
    Solution: Searching for a free port by testing a free port on the
    clients device instead of setting a hardcoded port. Giving the port as
    a parameter into spawn when starting the backend.
    
    Changes:
    - Implementation of the above behavior
    - Changing the app version to 0.2.0 in the package.json as this is a new
      feature

--- electron/src/omnaiBackend.ts
+++ electron/src/omnaiBackend.ts
@@ -2,9 +2,32 @@ import {spawn, ChildProcess } from "child_process";
 import {join} from "path"; 
 import { existsSync } from "fs-extra";
 import {app} from "electron"; 
+import * as net from 'net';
 
 export const omnaiscopeBackendManager = (()=> { // singelton for only one possible encapsulated instance of the backend 
     let backendProcess : ChildProcess | null = null; 
+
+    function testPort(port: number): Promise<boolean>{
+        return new Promise(resolve => {
+            const server = net.createServer(); 
+            server.unref(); //allows the programm to exit if this is the only server running 
+            server.once("error", () => resolve(false)); //first handle errors then listen to skip an internal error
+            server.listen(port, () => { 
+                server.close(() => resolve(true));
+            });
+        });
+    }
+
+    async function getFreePort() : Promise<number>{
+        let start : number = 3000; 
+        let end : number = 10000; 
+
+        for(let port : number = start; port <= end; port ++){
+            const isFree = await testPort(port); 
+            if (isFree) return port; 
+        }
+        throw new Error("No port available"); 
+    }
 
     function getBackendPath(): string {
         const exePath: string = app.isPackaged 
@@ -14,13 +37,15 @@ export const omnaiscopeBackendManager = (()=> { // singelton for only one possib
         return exePath; 
     }
 
-    function startBackend(): void {
+    async function startBackend(): Promise<void> {
         const exePath : string = getBackendPath(); 
 
+        const port : number = await getFreePort(); 
+
         if(existsSync(exePath)){
-            backendProcess = spawn(exePath, ["-w"]); 
-            console.log("Backend process started"); 
-        }
+            backendProcess = spawn(exePath, ["-w", "-p", port.toString()]); 
+            console.log(`Backend process started on port ${port}`);
+        }   
     }
 
     function stopBackend(): void {
