d5d58e44745fb75eacca1b3a1edf576c31f051fe
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2025-06-05 08:11:16 +0000

    add comments to csv file parsing

--- angular-frontend/src/app/omnai-datasource/csv-file-import/csv-file-import.service.ts
+++ angular-frontend/src/app/omnai-datasource/csv-file-import/csv-file-import.service.ts
@@ -34,35 +34,50 @@ export class CsvFileImportService implements DataSource {
    * @private
    */
   private async processFile(file: File):Promise<{name: string, out: DataFormat[]}> {
+    //Ensure, that the file is not empty (contains no lines)
     let text = await file.text();
     let lines = text.split('\n');
     if (lines.length < 1) throw CsvFileImportErrorKind.FileEmpty;
 
+    //The header of a file should be formed of 6 comma-seperated values:
+    //name,vin,kilometers,manufacturer,id,sampleRate
     let info = lines[0].split(',');
     if (info.length != 6) throw CsvFileImportErrorKind.InvalidHeader;
 
-    let samplingSpeed = Number(info[5]);
-    if (Number.isNaN(samplingSpeed) || !Number.isFinite(samplingSpeed))
+    //sampleRate is the 5th index in the list above
+    //name should be the id, which is the 4th index
+    let name = info[4];
+    let sampleRate = Number(info[5]);
+    if (Number.isNaN(sampleRate) || !Number.isFinite(sampleRate))
       throw CsvFileImportErrorKind.InvalidSamplingSpeed;
 
+    //Internally the Graph render converts everything to Date object.
+    //The Date object constructor takes a number and interprets it as milliseconds since the Unix Epoch.
+    //Therefore, the Date object cannot represent any timepoint, that doesn't lie exactly on one millisecond.
+    //
+    //This code calculates how many different millisecond values we can represent (length) and which items represent those milliseconds (keepEvery).
     let keepEvery = 1;
     let length = (lines.length-1);
-    if (samplingSpeed > 1000) {
-      keepEvery = samplingSpeed/1000;
-      length = length/keepEvery
+    if (sampleRate > 1000) {
+      keepEvery = sampleRate/1000;
+      length = Math.ceil(length/sampleRate*1000);
     }
 
-    let name = info[4];
-
-    let out = new Array(Math.ceil(length/samplingSpeed*1000));
+    //Pre-allocating the array, when you know the size is good practice.
+    //It makes it, so that data doesn't need to be moved, whilst adding more data
+    let out = new Array(length);
     let arrayIndex = 0;
+
+    // keep only every 1 data-value of keepEvery
+    // the first line is the header of the file, so we need to skip it
     for (let [index, sample] of lines.slice(1).entries()) {
       if (keepEvery !== 0 && index % keepEvery !== 0) continue;
       out[arrayIndex++] = {
-        timestamp: index/samplingSpeed*1000,
+        timestamp: index/sampleRate*1000,
         value: Number(sample),
       };
     }
+
     return {
       name,
       out,
