40774c97d90d2c58a3ffed2066fe75bafd31a100
Lukas Adrian Kron <l.kron@lugges.eu>
2025-06-08 18:33:11 +0300

    Update angular-frontend/src/app/omnai-datasource/omnai-scope-server/live-data.service.ts
    Co-authored-by: Tabea <Tabea.Roethemeyer@rub.de>
    Signed-off-by: Lukas Adrian Kron <l.kron@lugges.eu>

--- angular-frontend/src/app/omnai-datasource/omnai-scope-server/live-data.service.ts
+++ angular-frontend/src/app/omnai-datasource/omnai-scope-server/live-data.service.ts
@@ -64,7 +64,7 @@ export class OmnAIScopeDataService implements DataSource {
 
   readonly #httpClient = inject(HttpClient);
 
-  private getAvailableDevices(): void {
+  private setupDevicePolling(): void {
     const pollInterval_ms = 15 * 1000;
     timer(0, pollInterval_ms ) 
       .pipe(
