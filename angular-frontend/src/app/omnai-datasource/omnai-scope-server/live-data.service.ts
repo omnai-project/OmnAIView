cbb79f3d3b0a7db4c90787ae323e965063345b09
Lukas Adrian Kron <Lukas.Kron@ruhr-uni-bochum.de>
2025-06-17 14:22:46 +0200

    Fix merge

--- angular-frontend/src/app/omnai-datasource/omnai-scope-server/live-data.service.ts
+++ angular-frontend/src/app/omnai-datasource/omnai-scope-server/live-data.service.ts
@@ -30,7 +30,6 @@ interface DeviceOverview {
 })
 export class OmnAIScopeDataService implements DataSource {
 
-  private serverUrl = '127.0.0.1:8080';
   private readonly destroyRef = inject(DestroyRef);
 
   constructor() {
