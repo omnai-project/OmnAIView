2d97a211e6ba58deb4ce0f6b676638000572630d
AKMaily <83161524+AKMaily@users.noreply.github.com>
2025-07-10 13:00:17 +0200

    feat(mode): Add workshop/advanced mode
    Add workshop mode as default mode
    Add toggle: Switch between workshop and advanced mode
    
    Workshop mode: Start all connected OmnAIScopes directly

--- angular-frontend/src/app/source-selection/start-data-from-source.component.ts
+++ angular-frontend/src/app/source-selection/start-data-from-source.component.ts
@@ -4,7 +4,7 @@ import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { DataSourceSelectionService } from './data-source-selection.service';
 import { SourceSelectModalComponent } from './source-select-modal.component';
 import { MatIconModule } from '@angular/material/icon';
-
+import { AdvancedModeService } from '../advanced-mode/advanced-mode.service';
 
 @Component({
     selector: 'app-start-data-button',
@@ -19,17 +19,25 @@ import { MatIconModule } from '@angular/material/icon';
 export class StartDataButtonComponent {
     private readonly dialog = inject(MatDialog);
     private readonly datasource = inject(DataSourceSelectionService);
-
-    openModal() {
-        const dialogRef = this.dialog.open(SourceSelectModalComponent, {
-            width: '60vw'
-        });
-
-        dialogRef.afterClosed().subscribe(() => {
-            if (this.datasource.hasSelection()) {
-                // This should always be true as hasSelection is true when current Source is set
-                this.datasource.currentSource()?.connect();
+    private readonly advancedMode = inject(AdvancedModeService);
+    openModal(): void {
+        if (this.advancedMode.enabled()) {
+            const dialogRef = this.dialog.open(SourceSelectModalComponent, {
+                width: '60vw'
+            });
+            dialogRef.afterClosed().subscribe(() => {
+                if (this.datasource.hasSelection()) {
+                    this.datasource.currentSource()?.connect();
+                }
+            });
+            return;
+        }
+        else {
+            const OmnAIScope = this.datasource.availableSources().find(s => s.id === 'omnaiscope');
+            if (OmnAIScope) {
+                this.datasource.selectSource(OmnAIScope);
+                OmnAIScope.connect();
             }
-        });
+        }
     }
 }
