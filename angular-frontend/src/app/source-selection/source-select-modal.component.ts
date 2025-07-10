9ce251e71eb6fa5140457ce3a5d1a7bd988af53c
TenNex <76889841+TenNex161@users.noreply.github.com>
2025-07-10 12:56:24 +0200

    Enhancement: Update of source selection with material design components (#99)
    * Added Mat Card imports
    
    * Added html template for mat card
    
    * Added dark mode
    
    * Cleaned up code and added comments, deselect is missing.
    Added better version of dark mode.
    
    * Finished modal-select-menu in material design. Darkmode is also implemented, deselect of source and multiple sources are not implemented.
    
    * Removed dark-mode from custom-theme.scss
    
    * Improved comments
    
    * Added link for card styling
    
    * Moved css from source-select-modal.component.html to source-select-modal.component.css
    
    Changed html so mat-card layout is correctly implemented
    
    * Removed test class from source-select-modal.component.html
    
    ---------
    
    Co-authored-by: Ataneko161 <info@tentacle.nexus>

--- angular-frontend/src/app/source-selection/source-select-modal.component.ts
+++ angular-frontend/src/app/source-selection/source-select-modal.component.ts
@@ -2,13 +2,15 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef, MatDialogTitle } from '@angular/material/dialog';
 import { type DataSourceInfo, DataSourceSelectionService } from './data-source-selection.service';
+import {MatCardModule, MatCardHeader, MatCardContent, MatCardActions} from '@angular/material/card';
 
 @Component({
     selector: 'app-source-select-modal',
     standalone: true,
-    imports: [MatButtonModule, MatDialogActions, MatDialogClose, MatDialogTitle, MatDialogContent],
+    imports: [MatButtonModule, MatDialogActions, MatDialogClose, MatDialogTitle, MatDialogContent, MatCardModule, MatCardHeader, MatCardContent, MatCardActions],
     changeDetection: ChangeDetectionStrategy.OnPush,
     templateUrl: './source-select-modal.component.html',
+    styleUrls: ['./source-select-modal.component.css'],
 })
 export class SourceSelectModalComponent {
     private readonly datasourceService = inject(DataSourceSelectionService);
