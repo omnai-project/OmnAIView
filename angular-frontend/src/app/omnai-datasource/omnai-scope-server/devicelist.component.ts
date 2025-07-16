cad657ed701090aa957bdcafcb102255df1d94f2
franzwil <franz.wilfarth@mail.de>
2025-07-16 15:45:07 +0200

    Refactor devicelist component and html to not have a double stop button

--- angular-frontend/src/app/omnai-datasource/omnai-scope-server/devicelist.component.ts
+++ angular-frontend/src/app/omnai-datasource/omnai-scope-server/devicelist.component.ts
@@ -2,7 +2,6 @@
 
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { OmnAIScopeDataService } from './live-data.service';
-import { DummyDataService } from '../random-data-server/random-data.service';
 
 @Component({
     selector: 'app-device-list',
@@ -11,18 +10,7 @@ import { DummyDataService } from '../random-data-server/random-data.service';
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DeviceListComponent {
-    readonly #deviceHandler = inject(OmnAIScopeDataService);
-    readonly #randomDataHandler = inject(DummyDataService);
-    devices = this.#deviceHandler.devices
-    isLiveDataConnected = this.#deviceHandler.isConnected
-    isRandomDataConnected = this.#randomDataHandler.isConnected
-
-    getDevicesList = this.#deviceHandler.getDevices.bind(this.#deviceHandler)
-    disconnectSource = () => {
-        if (this.#deviceHandler.isConnected()) {
-            this.#deviceHandler.disconnect();
-        } else if (this.#randomDataHandler.isConnected()) {
-            this.#randomDataHandler.disconnect();
-        }
-    };
+    private readonly deviceHandler = inject(OmnAIScopeDataService);
+    protected readonly devices = this.deviceHandler.devices;
+    getDevicesList = this.deviceHandler.getDevices.bind(this.deviceHandler);
 }
