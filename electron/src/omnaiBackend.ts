ebd44b25628f5384b149558877789807e9197aec
AKMaily <83161524+AKMaily@users.noreply.github.com>
2025-07-18 08:52:08 +0200

    update(setup): Change name to OmnAIScopeBackend
    Change name of used application to OmnAIScopeBackend in electon
    omnaiBackend.ts .
    
    Name before : MiniOmni
    Name now : OmnAIScopeBackend

--- electron/src/omnaiBackend.ts
+++ electron/src/omnaiBackend.ts
@@ -1,7 +1,7 @@
-import {spawn, ChildProcess } from "child_process"; 
-import {join} from "path"; 
+import { spawn, ChildProcess } from "child_process";
+import { join } from "path";
 import { existsSync } from "fs";
-import {app} from "electron"; 
+import { app } from "electron";
 import * as net from 'net';
 
 /**
@@ -9,9 +9,9 @@ import * as net from 'net';
  * It provides a function to start and stop the backend, as well as receiving the port on which the backend currently runs. 
  * @version implements v0.5.1 of the backend interface 
  */
-export const omnaiscopeBackendManager = (()=> { // singelton for only one possible encapsulated instance of the backend 
-    let backendProcess : ChildProcess | null = null;
-    let port : number = 0;  
+export const omnaiscopeBackendManager = (() => { // singelton for only one possible encapsulated instance of the backend 
+    let backendProcess: ChildProcess | null = null;
+    let port: number = 0;
 
     /**
      * @description Typeguard for the AddressInfo of a port
@@ -20,16 +20,16 @@ export const omnaiscopeBackendManager = (()=> { // singelton for only one possib
      */
     function isAddressInfo(address: string | net.AddressInfo | null): address is net.AddressInfo {
         return typeof address === 'object' && address !== null && typeof address.port === 'number';
-    }    
+    }
     /**
      * @description gets a free port for the backend from the OS
      * @async
      * @usage should be used before starting the backend 
      * @returns 
      */
-    async function getFreePort() : Promise<number>{
+    async function getFreePort(): Promise<number> {
         return new Promise((resolve, reject) => {
-            const server = net.createServer(); 
+            const server = net.createServer();
             server.unref(); //allows the programm to exit if this is the only server running 
             server.once("error", err => reject(err)); //first handle errors then listen to skip an internal error
             server.listen(0, () => { // the OS gets an available port
@@ -49,11 +49,11 @@ export const omnaiscopeBackendManager = (()=> { // singelton for only one possib
      * @returns hardcoded path for production and dev mode in which the OmnAIScope Backend is currently saved 
      */
     function getBackendPath(): string {
-        const exePath: string = app.isPackaged 
-        ? join(process.resourcesPath, "MiniOmni.exe") // production mode 
-        : join(__dirname, "..", "res", "omnai_BE", "MiniOmni.exe") // dev mode 
+        const exePath: string = app.isPackaged
+            ? join(process.resourcesPath, "OmnAIScopeBackend.exe") // production mode 
+            : join(__dirname, "..", "res", "omnai_BE", "OmnAIScopeBackend.exe") // dev mode 
 
-        return exePath; 
+        return exePath;
     }
     /**
      * @description Starts the local omnai backend on a free port given by the OS
@@ -61,26 +61,26 @@ export const omnaiscopeBackendManager = (()=> { // singelton for only one possib
      * @async
      */
     async function startBackend(): Promise<void> {
-        const exePath : string = getBackendPath(); 
+        const exePath: string = getBackendPath();
 
-        port = await getFreePort(); 
+        port = await getFreePort();
 
-        if(existsSync(exePath)){
+        if (existsSync(exePath)) {
             backendProcess = spawn(exePath, ["-w", "-p", port.toString()], {
                 stdio: ['ignore', 'pipe', 'pipe'],
             });
             // read console buffer from BE because the backend does not delete the console buffer by itself
             // therefore the programm backend stops when the console buffer is full, temporary fix
-            backendProcess.stdout.on('data', () => {}); 
-            backendProcess.stderr.on('data', () => {});
+            backendProcess.stdout.on('data', () => { });
+            backendProcess.stderr.on('data', () => { });
         }
     }
     /**
      * @description Stops the omnai backend process if the process exists 
      */
     function stopBackend(): void {
-        if(backendProcess) {
-            backendProcess.kill(); 
+        if (backendProcess) {
+            backendProcess.kill();
             console.log("Backend process stopped");
         }
     }
@@ -88,12 +88,12 @@ export const omnaiscopeBackendManager = (()=> { // singelton for only one possib
      * @returns port of the omnaiscope backend if available , else 0
      */
     function getPort(): number {
-        return port; 
+        return port;
     }
 
     return {
-        startBackend, 
-        stopBackend, 
+        startBackend,
+        stopBackend,
         getPort
-    }; 
+    };
 })(); 
