65379162d8e4f691f5513b251cb4de1fffe5b6d9
franzwil <franz.wilfarth@mail.de>
2025-07-24 11:40:52 +0200

    feat(save): Add VisFeedback and csv format to dummy-data-service
    Add progress component and snack bar for message if saving was successful.
    Add csv file format with header for dummy data.

--- angular-frontend/src/app/omnai-datasource/dummy-data-server/dummy-data.service.ts
+++ angular-frontend/src/app/omnai-datasource/dummy-data-server/dummy-data.service.ts
@@ -5,6 +5,8 @@ import { DataSource } from '../../source-selection/data-source-selection.service
 import { DataFormat } from '../omnai-scope-server/live-data.service';
 import { MatDialog } from '@angular/material/dialog';
 import { SaveDataLocallyModalComponent } from '../../save-data-locally-modal/save-data-locally-modal.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { DownloadProgressComponent } from '../omnai-scope-server/downloadProgress.component';
 
 
 @Injectable({ providedIn: 'root' })
@@ -15,6 +17,8 @@ export class DummyDataService implements DataSource {
     readonly data = this._data.asReadonly(); 
     private subscription: Subscription | null = null;
     readonly isConnected = signal<boolean>(false);
+
+    constructor(private snackBar: MatSnackBar) {}
 
     connect(): void {
         if (this.subscription) return;
@@ -48,14 +52,25 @@ export class DummyDataService implements DataSource {
         if (!this.isConnected) {
             console.log('Dummy data source not connected.');
             return;
-        } else {
-            this.disconnect();
         }
         const dialogRef = this.dialog.open(SaveDataLocallyModalComponent, { width: '60vw' });
         dialogRef.afterClosed().subscribe(({dir, fileName}) => {
-            const dummyData = JSON.stringify(this._data());
-            if(window.electronAPI) window.electronAPI.saveFile(dummyData, dir, fileName);
-        })
+            if(window.electronAPI) {
+                const csv = ['# source: dummy data','# version: 1.0.0','timestamp,value', ...this._data()['dummy'].map(item => `${item.timestamp},${item.value}`)].join('\n');
+                const progressRef = this.dialog.open(DownloadProgressComponent, {
+                    disableClose: true
+                });
+                try {
+                    window.electronAPI.saveFile(csv, dir, fileName);
+                    this.snackBar.open('File sucessfully saved ✓', '', { duration: 4000 });
+                } catch {
+                    this.snackBar.open('Saving error', '', { duration: 4000 });
+                }
+                progressRef.close();
+            } else {
+                console.log('Electron app not activated');
+            }     
+        });
     }
     
     record(): void {
