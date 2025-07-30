b85d79b3fbc4c09590fd214561c335d908a38402
AKMaily <83161524+AKMaily@users.noreply.github.com>
2025-07-30 11:24:07 +0200

    fix(zoom): Add correct startpoint for relative time  (#141)
    * fix(zoom): Add correct startpoint for rel time
    
    Fix zoom behavior for relative time by using correct start time for
    x-axis-formatter-util function .
    
    Problem: Previously the relative time was calculated from the current
    date as well as the start date of the domain.
    Zooming changes the start data of the domain and therefore the relative
    time distance between the current date and the start date.
    This lead to wrongly calculated relative times when zooming.
    
    Solution: Receive minTimestamp reference when a measurement starts. Use
    reference to calculate relative time to.
    
    * Update package.json to v1.7.1

--- angular-frontend/src/app/graph/x-axis-formatter.utils.ts
+++ angular-frontend/src/app/graph/x-axis-formatter.utils.ts
@@ -2,10 +2,16 @@ import { type NumberValue, timeFormat } from "d3";
 
 export type xAxisMode = 'absolute' | 'relative';
 
+/**
+ * @brief Creates a tick-formatter for the X-Axis based on the mode 
+ * @param mode see {@link xAxisMode}
+ * @param startDate startdate relative time is calculated to  
+ * @returns tickformat function usable for d3
+ */
 export function makeXAxisTickFormatter(
   mode: xAxisMode,
-  domainStart: NumberValue | Date
-): (domainValue: NumberValue | Date, index: number) => string {
+  startDate: NumberValue | Date
+): (startValue: NumberValue | Date, index: number) => string {
   const formatAbsolute = timeFormat('%H:%M:%S');
   const formatRelative = timeFormat('%M:%S');
 
@@ -15,7 +21,7 @@ export function makeXAxisTickFormatter(
       return formatAbsolute(new Date(Number(d)));
     } else {
       const t = d instanceof Date ? d.getTime() : Number(d);
-      const t0 = domainStart instanceof Date ? domainStart.getTime() : Number(domainStart);
+      const t0 = startDate instanceof Date ? startDate.getTime() : Number(startDate);
       const elapsed = Math.max(0, t - t0);
       return formatRelative(new Date(elapsed));
     }
