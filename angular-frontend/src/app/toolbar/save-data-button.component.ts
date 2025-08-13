b33640031269ec867d72caa78636dfb33230932f
AKMaily <83161524+AKMaily@users.noreply.github.com>
2025-08-13 11:34:26 +0200

    ref(toolbar): Merge settings style with toolbar
    Merge settings button style with toolbar button style to apply a general
    style for all toolbar buttons.
    Imported MatIconButton to save/record/play button to apply general
    angular material style

--- angular-frontend/src/app/toolbar/save-data-button.component.ts
+++ angular-frontend/src/app/toolbar/save-data-button.component.ts
@@ -3,17 +3,18 @@ import { MatDialogModule } from "@angular/material/dialog";
 import { MatIconModule } from "@angular/material/icon";
 import { DataSourceSelectionService } from "../source-selection/data-source-selection.service";
 import { ToolbarState, ToolbarStateManagerService } from "./toolbarStateManager.service";
+import { MatIconButton } from '@angular/material/button';
 
 @Component({
     selector: 'app-save-data-button',
     standalone: true,
-    imports: [MatDialogModule, MatIconModule],
+    imports: [MatDialogModule, MatIconModule, MatIconButton],
     template: `
         <button mat-icon-button (click)="saveData()" aria-label="Save Data" id="save-button" [disabled]="this.toolbarState.getState() !== ToolbarState.STOPPED">
         <mat-icon>save</mat-icon>
         </button>
     `,
-    styles: `button { display: flex; padding: .3em }`,
+    styleUrl: './toolbar.component.css'
 })
 export class SaveDataButtonComponent {
     private readonly datasource = inject(DataSourceSelectionService);
